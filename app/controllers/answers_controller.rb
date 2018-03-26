e4cc6a747e6a6a861204e9fcbb5e3ea12268d0dd
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-26 15:51:40 +0200

    exo 1

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  # Let's fake a database records
+
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'questions/ask' precedemment
+  get 'ask', to: 'questions#ask', as: :ask
+
+  # get 'questions/answer' precedemment
+  get 'answer', to: 'questions#answer', as: :answer
+
+  get 'answers', to: 'answers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
